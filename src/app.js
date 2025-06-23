88bdf3b34e6412faadc941cb011515a20a18a7e3
devrajj02 <makwanad2004@gmail.com>
2025-06-23 16:02:07 +0530

    created server

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const app = express()
 app.use("/",(req,res)=>{
     res.send("Namastee")
 });
+app.use("/hello",(req,res)=>{
+    res.send("helloo")
+})
 app.listen(3333,()=>{
     console.log("Server Started Successfullyy")
 })
